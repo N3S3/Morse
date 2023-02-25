911f3aa2346b649f814a230ff110c78e5598e1bc
Sebastian Nestler <68975029+N3S3@users.noreply.github.com>
2023-02-25 04:42:28 +0000

    Update morse.py

--- morse.py
+++ morse.py
@@ -1,8 +1,8 @@
 import tkinter as tk
 import pyttsx3
 
-# Define the Morse code to German conversion function
-morse_to_german_dict = {
+# Define the Morse code to Latin conversion function
+morse_to_latin_dict = {
     ".-": "A",
     "-...": "B",
     "-.-.": "C",
@@ -41,28 +41,28 @@ morse_to_german_dict = {
     "-----": "0",
 }
 
-def morse_to_german(morse_code):
-    german_text = ""
+def morse_to_latin(morse_code):
+    latin_text = ""
     morse_words = morse_code.split("  ")
     for morse_word in morse_words:
         morse_letters = morse_word.split(" ")
         for morse_letter in morse_letters:
-            if morse_letter in morse_to_german_dict:
-                german_text += morse_to_german_dict[morse_letter]
+            if morse_letter in morse_to_latin_dict:
+                latin_text += morse_to_latin_dict[morse_letter]
             else:
-                german_text += "?"
-        german_text += " "
-    return german_text.strip()
+                latin_text += "?"
+        latin_text += " "
+    return latin_text.strip()
 
-# Define the German to Morse code conversion function
-german_to_morse_dict = {value: key for key, value in morse_to_german_dict.items()}
+# Define the Latin to Morse code conversion function
+latin_to_morse_dict = {value: key for key, value in morse_to_latin_dict.items()}
 
-def german_to_morse(german_text):
+def latin_to_morse(latin_text):
     morse_code = ""
-    german_text = german_text.upper()
-    for char in german_text:
-        if char in german_to_morse_dict:
-            morse_code += german_to_morse_dict[char] + " "
+    latin_text = latin_text.upper()
+    for char in latin_text:
+        if char in latin_to_morse_dict:
+            morse_code += latin_to_morse_dict[char] + " "
         elif char == " ":
             morse_code += " "
         else:
@@ -89,33 +89,33 @@ class MorseCodeConverterGUI:
         self.output_text.grid(row=3, column=0, padx=10, pady=5)
 
         # Create the conversion buttons
-        self.morse_to_german_button = tk.Button(master, text="Morse to German", command=self.convert_morse_to_german)
-        self.morse_to_german_button.grid(row=5, column=0, pady=5)
-        self.german_to_morse_button = tk.Button(master, text="German to Morse", command=self.convert_german_to_morse)
-        self.german_to_morse_button.grid(row=6, column=0, pady=5)
+        self.morse_to_latin_button = tk.Button(master, text="Morse to Latin", command=self.convert_morse_to_latin)
+        self.morse_to_latin_button.grid(row=5, column=0, pady=5)
+        self.latin_to_morse_button = tk.Button(master, text="Latin to Morse", command=self.convert_latin_to_morse)
+        self.latin_to_morse_button.grid(row=6, column=0, pady=5)
 
         # Create the text-to-speech checkbox
         self.text_to_speech_var = tk.IntVar()
-        self.text_to_speech_checkbox = tk.Checkbutton(master, text="Text-to-speech (Morse to German only)", variable=self.text_to_speech_var)
+        self.text_to_speech_checkbox = tk.Checkbutton(master, text="Text-to-speech (Morse to Latin only)", variable=self.text_to_speech_var)
         self.text_to_speech_checkbox.grid(row=7, column=0)
 
-    # Define the Morse to German conversion method
-    def convert_morse_to_german(self):
+    # Define the Morse to Latin conversion method
+    def convert_morse_to_latin(self):
         morse_code = self.input_text.get("1.0", "end-1c")
-        german_text = morse_to_german(morse_code)
+        latin_text = morse_to_latin(morse_code)
         self.output_text.delete("1.0", "end")
-        self.output_text.insert("1.0", german_text)
+        self.output_text.insert("1.0", latin_text)
 
         # Perform text-to-speech if the checkbox is checked
         if self.text_to_speech_var.get() == 1:
             engine = pyttsx3.init()
-            engine.say(german_text)
+            engine.say(latin_text)
             engine.runAndWait()
 
-    # Define the German to Morse conversion method
-    def convert_german_to_morse(self):
-        german_text = self.input_text.get("1.0", "end-1c")
-        morse_code = german_to_morse(german_text)
+    # Define the Latin to Morse conversion method
+    def convert_latin_to_morse(self):
+        latin_text = self.input_text.get("1.0", "end-1c")
+        morse_code = latin_to_morse(latin_text)
         self.output_text.delete("1.0", "end")
         self.output_text.insert("1.0", morse_code)
 
